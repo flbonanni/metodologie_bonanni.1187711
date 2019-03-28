3cccb50a78ec45ac5a8c1a66cd4b6bcd147cb413
Flavia <bonanni.1187711@studenti.uniroma1.it>
2019-03-28 11:23:25 +0100

    Add files via upload

---
+++ PuntiESegmenti/src/Segmento.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Flavia Bonanni
+ */
+
+public class Segmento
+{
+    Punto a;
+    Punto b;
+
+    /**
+     * 
+     * @param a inizio del segmento
+     * @param b fine del segmento
+     */
+    public Segmento(Punto a, Punto b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+}
